fe903eef9b3d65761239f8289008440eed3f779a
arham-chowdhry_ABT <arham.chowdhry@abbott.com>
2026-04-09 11:33:16 +0530

    deployment check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="flex items-center justify-around">
       <Tabs defaultValue="account" className="w-100">
         <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="account">Account Name</TabsTrigger>
           <TabsTrigger value="password">Password</TabsTrigger>
         </TabsList>
         <TabsContent value="account">
