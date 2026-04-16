bafcc9b974935b870323829ff80578903b0e8ad3
arham-chowdhry_ABT <arham.chowdhry@abbott.com>
2026-04-16 11:21:54 +0530

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import {
-  Tabs,
-  TabsList,
-  TabsTrigger,
-  TabsContent,
-} from "../components/ui/tabs";
 import Advice from "./Advice";
 
 export default function Home() {
@@ -13,18 +7,6 @@ export default function Home() {
     <>
       <div>Home</div>
       <Advice />
-      <Tabs defaultValue="overview" className="">
-        <TabsList>
-          <TabsTrigger value="overview">Overview</TabsTrigger>
-          <TabsTrigger value="analytics">Analytics</TabsTrigger>
-          <TabsTrigger value="reports">Reports</TabsTrigger>
-          <TabsTrigger value="settings">Settings</TabsTrigger>
-        </TabsList>
-        <TabsContent value="overview">overview</TabsContent>
-        <TabsContent value="analytics">analytics</TabsContent>
-        <TabsContent value="reports">reports</TabsContent>
-        <TabsContent value="settings">settings</TabsContent>
-      </Tabs>
     </>
   );
 }
